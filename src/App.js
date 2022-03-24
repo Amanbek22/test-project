6fbcf95ab37ff97b403e9c4e94844e6b94010313
Amanbek Asylbekov <een9.aman@gmail.com>
2022-03-24 19:37:35 +0600

    mapping and props

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import './App.css';
 import Header from "./components/header/Header"
 import Card from "./components/card/Card"
-
-function f(a, b) {
-  return a + b;
-}
-
-f(2, 2);
-f(2, 2);
-f(2, 2);
-f(2, 2);
-
+import Footer from "./components/footer/Footer"
 
 function App() {
+
+  const carsArray = [
+    {
+      title: "Продаю Audi", 
+      price: 5000, 
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minus modi deleniti atque dolore rerum.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/BMW_G11_IMG_2002.jpg/1200px-BMW_G11_IMG_2002.jpg"
+    },
+    {
+      title: "Продаю BMW", 
+      price: 4000, 
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minus modi deleniti atque dolore rerum.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/BMW_G11_IMG_2002.jpg/1200px-BMW_G11_IMG_2002.jpg"
+    },
+    {
+      title: "Продаю Honda", 
+      price: 8000, 
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minus modi deleniti atque dolore rerum.",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/BMW_G11_IMG_2002.jpg/1200px-BMW_G11_IMG_2002.jpg"
+    },
+  ]
+
   return (
     <div className="App">
       <Header />
-      {5 + 5}
       <div className='container mt-5'>
         <div className='row justify-content-between'>
-          <Card
-            title="Продаю Audi"
-            price={5000}
-            imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/BMW_G11_IMG_2002.jpg/1200px-BMW_G11_IMG_2002.jpg"
-          />
-
-          <Card
-            title="Продаю Audi"
-            price={5000}
-          />
-
+          {
+            carsArray.map((item) => <Card 
+              title={item.title} 
+              price={item.price}
+              imgUrl={item.img}
+              description={item.description}
+            />)
+          }
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
